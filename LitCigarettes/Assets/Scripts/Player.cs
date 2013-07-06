cecbaf0ce38b94b6477b42beb777763112c5da40
Christopher Wade <cjacobwade@gmail.com>
2013-07-06 01:54:25 -0400

    Basic FPS Controls working

--- LitCigarettes/Assets/Scripts/Player.cs
+++ LitCigarettes/Assets/Scripts/Player.cs
@@ -3,42 +3,118 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	
-	Vector3 moveDirection;
+	//Movement
+	CharacterController controller;
 	public int moveSpeed;
+	public Vector3 moveDirection;
+	public int jumpSpeed;
+	public float gravitySpeed;
+	public float ySpeed;
+	//Camera Control
+	public int rotateSpeed;
+		//Up/Down
+			public float cameraV;//how much are we currently rotating
+			float cameraRotationV;
+			public float minCameraV;
+			public float maxCameraV;
+		//Left/Right
+			public float cameraH;
+	
+	
+	
+	
+	
+
+	
 	
 	// Use this for initialization
 	void Start () 
 	{
-	
+		controller = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		MovementInput();
-		CameraControl();
-		transform.Translate(moveDirection*Time.deltaTime);
+		//MovementInput();
+		Movement();
+		MouseInput();
+		if (Input.GetKeyDown(KeyCode.Escape))
+        	Screen.lockCursor = false;
+		//transform.Translate(moveDirection*Time.deltaTime);
+		controller.Move(moveDirection*Time.deltaTime);
 	}
 	
-	void CameraControl()
+	void MouseInput()
 	{
+		LeftMouse();
+		CameraHorizontal();
+		CameraVertical();
+	}
+	
+	void LeftMouse()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Screen.lockCursor = true;
+			Screen.showCursor = false;
+		}
+	}
+	
+	void CameraHorizontal()
+	{
+		cameraH = Input.GetAxis("Mouse X");
+		transform.Rotate(new Vector3(0,Time.deltaTime*cameraH*rotateSpeed,0));
+		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,0);
+	}
+	
+	void CameraVertical()
+	{
+		if(Input.GetAxis("Mouse Y")>rotateSpeed)
+			cameraV=rotateSpeed;
+		else if(Input.GetAxis("Mouse Y")<-rotateSpeed)
+			cameraV=-rotateSpeed;
+		else
+			cameraV = Input.GetAxis("Mouse Y");
 		
+		if(cameraRotationV >= minCameraV && cameraRotationV <= maxCameraV)
+		{
+			cameraRotationV += cameraV;
+			transform.Rotate(new Vector3(Time.deltaTime*cameraV*-rotateSpeed/*change this to positive for reversed axis*/,0,0));
+		}
+		if(cameraRotationV < minCameraV)//if lower than min rotation, correct
+		{
+			cameraRotationV += 1;
+			transform.Rotate(new Vector3(Time.deltaTime*cameraV*-rotateSpeed,0,0));
+		}
+		if(cameraRotationV > maxCameraV)//if higher than max rotation, correct
+		{
+			cameraRotationV -= 1;
+			transform.Rotate(new Vector3(Time.deltaTime*cameraV*-rotateSpeed,0,0));
+		}	
 	}
 	
-	void MovementInput()
+	void Movement()
 	{
-		#region WASD
-			if(Input.GetKey(KeyCode.W))
-				transform.Translate(new Vector3(0,0,moveSpeed)*Time.deltaTime);
-			
-			if(Input.GetKey(KeyCode.S))
-				transform.Translate(new Vector3(0,0,-moveSpeed*.8f)*Time.deltaTime);
-			
-			if(Input.GetKey(KeyCode.A))
-				transform.Translate(new Vector3(-moveSpeed*.7f,0,0)*Time.deltaTime);	
-			
-			if(Input.GetKey(KeyCode.D))
-				transform.Translate(new Vector3(moveSpeed*.7f,0,0)*Time.deltaTime);
-		#endregion
+		//Horizontal and Vertical axis are controlled by wasd or arrows
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;	
+		moveDirection = new Vector3(moveDirection.x,ySpeed,moveDirection.z);
+		if(controller.isGrounded)
+		{
+			if(Input.GetButton("Jump"))
+			{
+				ySpeed = 0;
+				ySpeed += jumpSpeed;
+			}
+			else
+				moveDirection.y = 0;
+		}
+		else
+		{
+			if(ySpeed > -9.8)
+				ySpeed += gravitySpeed;
+		}
 	}
 }
